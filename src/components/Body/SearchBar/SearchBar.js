06911873d10aba70ae3f2992ffd3ecc237be75dc
Your Name <you@example.com>
2022-03-27 02:28:14 -0700

    add srcbar

--- src/components/Body/SearchBar/SearchBar.js
+++ src/components/Body/SearchBar/SearchBar.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import {AiOutlineSearch} from 'react-icons/ai'
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div>
+      <div class="input-group m-3 w-75 p-2 mx-auto">
+        <div className=" w-75 mx-auto">
+       <div className="">
+       <input type="name" name="" id=" " className="w-75 p-1" placeholder="search food" />
+          <AiOutlineSearch className="ms-2 fs-3"/>
+       </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
