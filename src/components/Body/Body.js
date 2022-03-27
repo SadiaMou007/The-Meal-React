eb15a50b07a489024e50625e7207252de2b72218
Your Name <you@example.com>
2022-03-27 00:12:00 -0700

    first commit

---
+++ src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+import SearchBar from './SearchBar/SearchBar'
+const Body = () => {
+  return (
+    <div>
+    <SearchBar></SearchBar>
+    
+    </div>
+  )
+}
+
+export default Body
