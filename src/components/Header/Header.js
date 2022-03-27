167a6225a1c9e572476cd3c4a7ab125c4992f95b
Your Name <you@example.com>
2022-03-27 01:58:13 -0700

    header add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FcLike } from "react-icons/fc";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <nav class="navbar navbar-light bg-light navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand text-warning fw-bold" href="./home">
+            The Meal
+          </a>
 
-export default Header
+          <div class="me-2">
+            <span type="button" class=" position-relative fs-5 me-3">
+              <FaShoppingCart />
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill p-1 b-main">
+                <small className="b-counter">0</small>
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </span>
+
+            <FcLike className="me-3 fs-5" />
+            <FaUserAlt className="me-3 fs-5" />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
